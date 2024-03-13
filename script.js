6672827878b8f9e8e1b3eb9e9a5e3d1e13b0dde7
jitanshu <126978458+jitanshuag@users.noreply.github.com>
2024-03-13 22:01:41 +0530

    added assistant and also some  more pages

--- script.js
+++ script.js
@@ -15,5 +15,19 @@ app.get('/fluidflow', (req, res) => {
 app.get('/index', (req, res) => {
   res.render("index")
 })
-
+app.get('/chemicalkinetics' , (req,res)=>{
+  res.render("chemicalkinetics")
+})
+app.get('/distillation' , (req,res)=>{
+  res.render("distillation")
+})
+app.get('/masstransfer' , (req,res)=>{
+  res.render("masstransfer")
+})
+app.get('/heattransfer' , (req,res)=>{
+  res.render("heattransfer")
+})
+app.get('/thermodynamics' , (req,res)=>{
+  res.render("thermodynamics")
+})
 app.listen(3000)
